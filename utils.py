7539f19530355b1f243eb2523b3ca7c9835cba70
Matthijs de Goede <m.b.j.degoede@student.tudelft.nl>
2024-09-14 17:41:30 +0200

    Add code to generate graphs and calculate wake distances

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
+import os
+import networkx as nx
 import numpy as np
-from scipy.interpolate import griddata
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+from numpy.linalg import norm
+from scipy.interpolate import griddata
 
 
 def import_vtk(file):
@@ -99,6 +104,50 @@ def vtk_to_umean_abs(file):
     return umean_abs, x_axis, y_axis
 
 
+def get_file_name(type, i):
+    if type == 'BL':
+        return f'./slices/Processed/{type}/Windspeed_map_scalars_{i}.npy'
+    return f'./slices/{type}/Windspeed_map_scalars_{i}.npy'
+
+
+def animate_mean_absolute_speed(start, frames=None):
+    if frames is None:
+        all_files = os.listdir('./slices/Processed/BL/')
+        biggest_file = int(
+            max(all_files, key=lambda x: int(x.split('.')[0].split('_')[-1])).split('.')[0].split('_')[-1])
+        frames = (biggest_file - start) // 5 + 1
+        print(frames)
+
+    images = []
+
+    def setup_image(ax, type):
+        umean = np.load(get_file_name(type, start))
+        axes_image = ax.imshow(umean, animated=True)
+        ax.set_xlabel("Distance (m)")
+        ax.set_ylabel("Distance (m)")
+        images.append((axes_image, type))
+
+    fig, (ax1) = plt.subplots(1, 1)
+
+    setup_image(ax1, "BL")
+
+    # setup_image(ax2, "LuT2deg")
+    # fig.colorbar(axes_image_1)
+
+    def animate(i):
+        fig.suptitle(f"Interpolated UmeanAbs at Hub-Height\nt = {5 * i} seconds")
+
+        for axes_image, type in images:
+            umean_abs = np.load(get_file_name(type, start + 5 * i))
+            axes_image.set_data(umean_abs)
+        return fig, *images
+
+    anim = animation.FuncAnimation(fig=fig, func=animate, frames=frames, interval=50)
+    os.makedirs(f'./animations/{start}', exist_ok=True)
+    progress_callback = lambda i, n: print(f'Saving frame {i}/{n}, slice {start + 5 * i}')
+    anim.save(f'./animations/{start}/{frames}.gif', writer='pillow', progress_callback=progress_callback)
+
+
 def plot_mean_absolute_speed(umean_abs, x_axis, y_axis):
     """"
     Plots the mean absolute wind speed over the given grid
@@ -115,6 +164,89 @@ def plot_mean_absolute_speed(umean_abs, x_axis, y_axis):
     plt.show()
 
 
-umean_abs, x_axis, y_axis = vtk_to_umean_abs(
-    '../measurements_flow/postProcessing_BL/sliceDataInstantaneous/30890/U_slice_horizontal.vtk')
-plot_mean_absolute_speed(umean_abs, x_axis, y_axis)
+def angle_to_vec(wind_angle):
+    angle_radians = np.deg2rad(wind_angle)
+    return np.array([np.cos(angle_radians), np.sin(angle_radians)])
+
+
+def read_wind_angles(file):
+    return np.genfromtxt(file, delimiter=",")
+
+
+def get_wind_vec_at_time(wind_angles, timestep):
+    return angle_to_vec(wind_angles[wind_angles[:, 0] < timestep][-1, 1])
+
+
+def read_turbine_positions(file):
+    return np.genfromtxt(file, delimiter=",")[:, :2]
+
+
+def get_angle_between_vec(v1, v2):
+    return np.degrees(np.arccos(np.clip(np.dot(v1, v2) / (norm(v1) * norm(v2)), -1.0, 1.0)))
+
+
+def create_turbine_graph(pos, wind_vec, max_angle=90, max_dist=np.inf):
+    G = nx.DiGraph()
+    num_turbines = pos.shape[0]
+    for i in range(num_turbines):
+        G.add_node(i, pos=pos[i])
+
+    for i in range(num_turbines):
+        for j in range(num_turbines):
+            if i != j:
+                turb_vec = pos[j, :] - pos[i, :]
+                angle = get_angle_between_vec(turb_vec, wind_vec)
+                dist = norm(turb_vec)
+                if dist < max_dist and angle < max_angle:
+                    # add edge i to j
+                    edge_feat = calculate_wake_distances(turb_vec, wind_vec, angle=angle)
+                    G.add_edge(i, j, edge_feat=edge_feat)
+    return G
+
+
+def plot_graph(G, wind_vec, max_angle=90):
+    pos_dict = nx.get_node_attributes(G, 'pos')
+    plt.figure(figsize=(10, 8))
+    # Draw nodes
+    nx.draw_networkx_nodes(G, pos_dict, node_size=500, node_color='lightblue')
+    # Draw edges
+    nx.draw_networkx_edges(G, pos_dict, edgelist=G.edges(), arrowstyle='-|>', arrowsize=20)
+    # Draw node labels
+    nx.draw_networkx_labels(G, pos_dict, font_size=12, font_family='sans-serif')
+    # Draw wind direction
+    wind_start = np.mean(np.array(list(pos_dict.values())), axis=0)
+    scaled_wind_vec = 1000 * wind_vec
+    plt.quiver(wind_start[0], wind_start[1], scaled_wind_vec[0], scaled_wind_vec[1],
+               angles='xy', scale_units='xy', scale=1, color='red', label='Wind Direction')
+    plt.legend()
+    plt.title(f"Turbine Graph with Wind Direction (max angle: {max_angle})")
+    plt.xlabel("X Coordinate")
+    plt.ylabel("Y Coordinate")
+    plt.grid(True)
+    plt.axis("equal")
+    plt.show()
+
+
+def calculate_wake_distances(turb_vec, wind_vec, angle=None):
+    if angle is None:
+        angle = get_angle_between_vec(turb_vec, wind_vec)
+
+    angle_rad = np.deg2rad(angle)
+    dist_i_j = norm(turb_vec)
+    rad_dist = dist_i_j * np.sin(angle_rad)
+    down_str_dist = dist_i_j * np.cos(angle_rad)
+    return rad_dist, down_str_dist
+
+
+if __name__ == "__main__":
+    wind_angles = read_wind_angles("HKN_12_to_15_dir.csv")
+    turbine_pos = read_turbine_positions("HKN_12_to_15_layout_balanced.csv")
+    timestep = 1310
+    max_angle = 90
+    wind_vec = get_wind_vec_at_time(wind_angles, timestep)
+    graph = create_turbine_graph(turbine_pos, wind_vec, max_angle=max_angle)
+    plot_graph(graph, wind_vec, max_angle=max_angle)
+    # animate_mean_absolute_speed(30005)
+    # umean_abs, x_axis, y_axis = vtk_to_umean_abs(
+    #     '../measurements_flow/postProcessing_BL/sliceDataInstantaneous/30890/U_slice_horizontal.vtk')
+    # plot_mean_absolute_speed(umean_abs, x_axis, y_axis)
