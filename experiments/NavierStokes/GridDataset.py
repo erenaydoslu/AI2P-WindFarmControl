02aed7a65c7dee585a73969cdc90fda6d2684d00
Eren Aydoslu <erenaydoslue@icloud.com>
2024-10-07 18:32:10 +0200

    Added functionality for adaptive sampling

--- experiments/NavierStokes/GridDataset.py
+++ experiments/NavierStokes/GridDataset.py
@@ -8,21 +8,21 @@ import pandas as pd
 
 
 class GridDataset(Dataset):
-    def __init__(self, dir, turbine_csv, wind_csv, data_type="no-wake", wake_dir=None, wake_turbine_csv=None, only_grid_values=False):
+    def __init__(self, dir, turbine_csv, wind_csv, data_type="no-wake", wake_dir=None, wake_turbine_csv=None, only_grid_values=False, sampling=False, samples_per_grid=256):
         super().__init__()
 
         self.datasets = []
         self.data_type = data_type
 
         if (self.data_type == "no-wake"):
-            self.datasets.append(CaseDataset(dir, turbine_csv, wind_csv, only_grid_values))
+            self.datasets.append(CaseDataset(dir, turbine_csv, wind_csv, only_grid_values, sampling, samples_per_grid))
 
         elif (self.data_type == "wake"):
-            self.datasets.append(CaseDataset(wake_dir, wake_turbine_csv, wind_csv, only_grid_values))
+            self.datasets.append(CaseDataset(wake_dir, wake_turbine_csv, wind_csv, only_grid_values, sampling, samples_per_grid))
 
         elif (self.data_type == "both"):
-            self.datasets.append(CaseDataset(dir, turbine_csv, wind_csv, only_grid_values))
-            self.datasets.append(CaseDataset(wake_dir, wake_turbine_csv, wind_csv, only_grid_values))
+            self.datasets.append(CaseDataset(dir, turbine_csv, wind_csv, only_grid_values, sampling, samples_per_grid))
+            self.datasets.append(CaseDataset(wake_dir, wake_turbine_csv, wind_csv, only_grid_values, sampling, samples_per_grid))
             
         else:
             raise ValueError(f"data_type must be no-wake, wake, or both. was given: {self.data_type}")
@@ -50,7 +50,7 @@ class GridDataset(Dataset):
     
 
 class CaseDataset(Dataset):
-    def __init__(self, dir, turbine_csv, wind_csv, only_grid_values=False):
+    def __init__(self, dir, turbine_csv, wind_csv, only_grid_values=False, sampling=False, samples_per_grid=256):
         super().__init__()
 
         self.dir = dir
@@ -69,7 +69,10 @@ class CaseDataset(Dataset):
         self.coords = torch.stack([x_coords, y_coords]).T
 
         self.df_turbines = pd.read_csv(turbine_csv, index_col=0)
-        self.df_wind = pd.read_csv(wind_csv, index_col=0)       
+        self.df_wind = pd.read_csv(wind_csv, index_col=0)
+
+        self.sampling = sampling
+        self.samples_per_grid = samples_per_grid
 
     def __len__(self):
         return len(self.files) 
@@ -81,20 +84,71 @@ class CaseDataset(Dataset):
 
     def get_wind_data(self, time):
         time_instance = self.df_wind[self.df_wind['time'] == time][['winddir_sin', 'winddir_cos']]
-        return torch.from_numpy(time_instance.to_numpy())
+        return torch.from_numpy(time_instance.to_numpy()) 
     
+
+    def _calculate_vorticity(self, flow_field):
+        """
+        Vorticity (ω) describes the local spinning motion of a fluid.
+        ωx: dw/dy - dv/dw
+        ωy: du/dz - dw/dx
+        ωz: dv/dx - du/dy
+        |ω|: sqrt(ωx**2 + ωy**2 + ωz**2)
+        """
+        du = torch.gradient(flow_field[:, :, 0])
+        dv = torch.gradient(flow_field[:, :, 1])
+        dw = torch.gradient(flow_field[:, :, 2])
+
+        dudx, dudy = du[0], du[1]
+        dvdx, dvdy = dv[0], dv[1]
+        dwdx, dwdy = dw[0], dw[1]
+
+        x_vorticity = dwdy
+        y_vorticity = -dwdx
+        z_vorticity = dvdx - dudy
+
+        vorticity_intensity = np.sqrt(x_vorticity ** 2 + y_vorticity **2 + z_vorticity ** 2)
+        return vorticity_intensity
+
+
+    def _get_sampled_indices(self, vorticity):
+        """
+        The model struggles the most with high vorticity areas. We assign a
+        higher selection ratio to the top 20% vorticity areas for sampling.
+        As a result with a sample size of 256, 128 of them should be from the
+        top %20 vorticity and 128 of them should be from the rest of grid.
+        """
+        vorticity = vorticity.flatten()
+        threshold = torch.quantile(vorticity, 0.8)
+        selection_odds = np.array(torch.where(vorticity > threshold, 1, 4))
+
+        indices = np.random.choice(range(vorticity.size), 
+                                   size=self.samples_per_grid, 
+                                   replace=False, 
+                                   p=selection_odds/selection_odds.sum())
+        
+        return list(indices)
+        
 
     def __getitem__(self, index):
         if (isinstance(index, int) or isinstance(index, np.int32)):
-            #3x300x300
-            flow_field = torch.from_numpy(np.load(f"{self.dir}{self.files[index]}"))
-            flow_field = flow_field.permute(1, 2, 0).flatten(0, 1) #90000x3
+            #3x300x300 to 300x300x3
+            flow_field = torch.from_numpy(np.load(f"{self.dir}{self.files[index]}")).permute(1, 2, 0)
+
+            if (self.sampling):
+                vorticity = self._calculate_vorticity(flow_field)
+                indices = self._get_sampled_indices(vorticity)
+
+            flow_field = flow_field.flatten(0, 1) #90000x3
 
             time = int(self.files[index].split(".")[0])
             time_tensor = torch.full((self.coords.shape[0], 1), time)
 
             if (self.only_grid_values):
                 inputs = torch.concat((self.coords, time_tensor), dim=1)
+                if (self.sampling):
+                    return inputs[indices], flow_field[indices]
+                
                 return inputs, flow_field
 
             turbine_data = self.get_turbine_data(time)
@@ -105,82 +159,11 @@ class CaseDataset(Dataset):
 
             inputs = torch.concat((self.coords, time_tensor, turbine_data, wind_data), dim=1)
 
+            if (self.sampling):
+                return inputs[indices], flow_field[indices]
+
             return inputs, flow_field        
 
         else:
             raise TypeError(f"{type(index)}")
 
-
-
-
-class RandomGridDataset(Dataset):
-    def __init__(self, dir, samples_per_grid=64, selection_ratio=0.5, grid_size=(300,300)):
-        """
-        Args:
-            dir: base path that contains the data
-            samples_per_grid: number of randomly selected samples per grid
-            selection_ratio: the ratio of samples to be selected per iteration 
-                over the dataset with respect to all samples
-        """
-        super().__init__()
-        self.dir = dir
-        self.files = os.listdir(self.dir)
-        self.total_grids = len(self.files)
-
-        self.samples_per_grid = samples_per_grid
-        self.selection_ratio = selection_ratio
-        self.total_samples_per_grid = grid_size[0] * grid_size[1]
-        self.selection_per_file = int(self.total_samples_per_grid * self.selection_ratio)
-
-    def __len__(self):
-        total_samples = self.total_grids * self.total_samples_per_grid
-        return int(total_samples * self.selection_ratio)
-    
-
-    def index_to_file_index(self, index):
-        if (index >= 0):
-            return index // self.selection_per_file
-        
-        return self.index_to_file_index(self.__len__() + index)
-    
-
-    def __getitem__(self, index):
-        if (isinstance(index, int)):
-            index = self.index_to_file_index(index)
-
-            grid = torch.from_numpy(np.load(f"{self.dir}{self.files[index]}"))
-
-            random_coords = torch.randint(0, 300, (self.samples_per_grid, 2))
-            x_indices = random_coords[:, 0]
-            y_indices = random_coords[:, 1]
-
-            sampled_grid = grid[:, x_indices, y_indices].T
-
-            time = int(self.files[index].split(".")[0])
-            time_tensor = torch.full_like(x_indices, time).unsqueeze(-1)
-
-            inputs = torch.concat((random_coords, time_tensor), dim=1)
-
-            return inputs, sampled_grid
-        
-        elif (isinstance(index, torch.Tensor)):
-            print("shit")
-            if (len(index.shape) > 1):
-                raise ValueError(f"index tensor must be 1D, got {index.shape}")
-            
-            inputs, sampled_grids = zip(*(self.__getitem__(idx.item()) for idx in index))
-
-            return torch.concatenate(inputs), torch.concatenate(sampled_grids)
-        
-        else:
-            raise TypeError(f"{type(index)}")        
-
-
-
-
-            
-
-
-
-
-    
