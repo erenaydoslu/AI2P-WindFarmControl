8021e7a317f1cbe7d7f4b6e42233a0e035ab1c63
ekemmeren <e.a.kemmeren@student.tudelft.nl>
2024-10-23 11:28:54 +0200

    Add eval callback

--- utils/sb3_callbacks.py
+++ utils/sb3_callbacks.py
@@ -1,6 +1,12 @@
+from typing import Union
+
+import numpy as np
+from gymnasium import Env
 from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.logger import Figure
 import matplotlib.pyplot as plt
+from stable_baselines3.common.vec_env import VecEnv
+
 
 class FigureRecorderCallback(BaseCallback):
     def __init__(self, env, verbose=0):
@@ -13,3 +19,26 @@ class FigureRecorderCallback(BaseCallback):
         self.logger.record("trajectory/figure", Figure(fig, close=True), exclude=("stdout", "log", "json", "csv"))
         plt.close()
         return True
+
+class ComparisonCallback(BaseCallback):
+    def __init__(self, eval_env: Union[Env, VecEnv], verbose=0):
+        super().__init__(verbose)
+        self.eval_env = eval_env
+
+    def _on_step(self):
+        seed = 42
+        self.eval_env.reset(seed=seed)
+        greedy = np.ones(10) * 7 # Actions is a discrete space, where 7 is the middle
+        _, rewards_greedy, _, _, info_greedy = self.eval_env.step(greedy)
+        self.eval_env.render()
+
+        # Model control
+        obs, info = self.eval_env.reset(seed=seed)
+        action, states = self.model.predict(obs)
+        _, rewards_model, _, _, info_model = self.eval_env.step(action)
+        print(f"rewards_greedy: {rewards_greedy}, rewards_model: {rewards_model}")
+        print(f"info_greedy: {info_greedy['windspeed']}, info_model: {info_model['windspeed']}")
+        self.eval_env.render()
+        return True
+
+
