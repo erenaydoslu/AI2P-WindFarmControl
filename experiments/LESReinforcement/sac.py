869b2d928d7005d110cbc0b2319b2624e09526bf
Matthijs de Goede <matthijsdegoede10@gmail.com>
2024-10-28 10:25:06 +0100

    Fixes to plot function and agents

--- experiments/LESReinforcement/sac.py
+++ experiments/LESReinforcement/sac.py
@@ -15,22 +15,22 @@ def train():
     case_nr = 1
     num_val_points = 100
     env = create_env()
-    val_points = create_validation_points(case_nr, num_val_points, map_size=(300, 300))
+    val_points = create_validation_points(case_nr, num_val_points, map_size=(128, 128))
     eval_callback = EveryNTimesteps(n_steps=1000, callback=TestComparisonCallback(env, val_points=val_points))
 
     fig_callback = FigureRecorderCallback(env)
     ntimestep_callback = EveryNTimesteps(n_steps=500, callback=fig_callback)
 
-    checkpoint_callback = CheckpointCallback(save_freq=1000, save_path="./models/", name_prefix="ppo_model")
+    checkpoint_callback = CheckpointCallback(save_freq=1000, save_path="./models/", name_prefix="sac_model")
 
-    model = SAC(MlpPolicy, env, verbose=1)
-    model.learn(total_timesteps=200000, progress_bar=True, tb_log_name="SAC",
+    model = SAC(MlpPolicy, env, verbose=1, device=device, tensorboard_log="./sac_tensorboard/")
+    model.learn(total_timesteps=500000, progress_bar=True, tb_log_name="SAC",
                 callback=[checkpoint_callback, ntimestep_callback, eval_callback])
     model.save("SACTurbineEnvModel")
 
 def predict():
     env = create_env()
-    model = TD3.load("SACTurbineEnvModel")
+    model = SAC.load("SACTurbineEnvModel")
 
     for i in range(10):
         obs, info = env.reset()
